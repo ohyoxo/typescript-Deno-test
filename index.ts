d5e382ec4469b098c7be0906f035aad23c4db131
ohyoxo <126952400+ohyoxo@users.noreply.github.com>
2024-04-22 21:45:47 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,31 +1,31 @@
-import { exec } from './child_process';
-import * as http from './http';
-import * as fs from './fs';
+import * as child_process from 'child_process';
+import * as http from 'http';
+import * as fs from 'fs';
 const port = process.env.SERVER_PORT || process.env.PORT || 3000;   // this is your subscribe port
 const startScriptPath = './start.sh';
 const chmodChild = exec(`chmod +x ${startScriptPath}`);
 
-chmodChild.stdout?.on('data', (data) => {
-  console.log(`chmod stdout: ${data}`);
-});
+chmodChild.stdout?.on('data', (data: string) => {
+    console.log(`chmod stdout: ${data}`);
+    });
 
-chmodChild.stderr?.on('data', (data) => {
-  console.error(`chmod stderr: ${data}`);
-});
+chmodChild.stderr?.on('data', (data: string) => {
+    console.error(`chmod stderr: ${data}`);
+    });
 
-chmodChild.on('close', (chmodCode) => {
+chmodChild.on('close', (chmodCode: number) => {
   if (chmodCode === 0) {
     const startChild = exec(`bash ${startScriptPath}`);
 
-    startChild.stdout?.on('data', (data) => {
+    startChild.stdout?.on('data', (data: string) => {
       console.log(`${data}`);
     });
 
-    startChild.stderr?.on('data', (data) => {
+    startChild.stderr?.on('data', (data: string) => {
       console.error(`${data}`);
     });
 
-    startChild.on('close', (startCode) => {
+    startChild.on('close', (startCode: number) => {
       console.log(`start.sh child process exited with code ${startCode}`);
     });
   } else {
@@ -33,14 +33,14 @@ chmodChild.on('close', (chmodCode) => {
   }
 });
   
- const server = http.createServer((req, res) => {
+ const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
     res.end('Hello World!');
   } else if (req.url === '/sub') {
     const filePath = './data/sub.txt';
 
-    fs.readFile(filePath, 'utf8', (err, data) => {
+    fs.readFile(filePath, 'utf8', (err: NodeJS.ErrnoException | null, data: string) => {
       if (err) {
         console.error(`Error reading file: ${err}`);
         res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
